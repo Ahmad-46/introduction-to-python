82215dc48d345cda9a4c508989ceb5d24f5f905c
Ahmad-46 <mubbaranauman@gmail.com>
2025-04-24 09:11:52 -0700

    hello

---
+++ main.py
@@ -0,0 +1,9 @@
+#I am a comment in python
+# what is comment in python: a comment is basically a text note that gives an explanation about a specific line of code furthermore,they act as documentation in the source code
+#how to create a variable in python
+#it is basically a place in the computer memory which has a name 
+age = 14 # variable integer defined
+first_name = "Ahmad" #string type variable defining
+print(f"{first_name} is {age} year old.")
+print(f"{first_name} loves football and coding.")
+print(F"{first_name} reads in 9th class.")
